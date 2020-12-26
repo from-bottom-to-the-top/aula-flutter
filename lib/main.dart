4b94a1726340dd46e2651d43378fba4dd87955fa
Renaldo Mateus <renaldomateus77.dev@gmail.com>
2020-12-26 03:24:46 +0100

    Image and Row widget add

--- lib/main.dart
+++ lib/main.dart
@@ -80,15 +80,33 @@ class BaseWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.yellow,
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[ButtonWithIcon(), ButtonWithText()],
-        ),
-      ),
-    );
+        color: Colors.yellow,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              ButtonWithIcon(),
+              ButtonWithText(textButton: Text("Click me")),
+              ButtonWithText(textButton: Text("Ok")),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Container(width: 50, height: 50, color: Colors.red),
+                  Container(width: 50, height: 50, color: Colors.green),
+                  Container(width: 50, height: 50, color: Colors.blue),
+                ],
+              ),
+              Image.asset('assets/pockemon.jpeg',
+                  width: MediaQuery.of(context).size.width * 0.3,
+                  height: MediaQuery.of(context).size.height * 0.20),
+              Image.network(
+                  "https://i.ibb.co/HCf1jXg/Pok-mon-Gold-And-Silver-Pikachu-PNG-Free-Download.jpg",
+                  width: MediaQuery.of(context).size.width * 0.3,
+                  height: MediaQuery.of(context).size.height * 0.20)
+            ],
+          ),
+        ));
   }
 }
 
@@ -116,20 +134,20 @@ class ButtonWithIcon extends StatelessWidget {
 }
 
 class ButtonWithText extends StatelessWidget {
+  final Text textButton;
+  ButtonWithText({this.textButton});
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
       child: Container(
-        transform: Matrix4.identity()
-          ..scale(.9)
-          ..translate(9.0, 4.0, 13.0),
-        color: Colors.blue,
-        width: double.infinity,
-        height: 50,
-        alignment: Alignment.center,
-        child: Text("Click-me",
-            style: TextStyle(fontStyle: FontStyle.italic, color: Colors.white)),
-      ),
+          transform: Matrix4.identity()
+            ..scale(.9)
+            ..translate(9.0, 4.0, 13.0),
+          color: Colors.blue,
+          width: double.infinity,
+          height: 50,
+          alignment: Alignment.center,
+          child: textButton),
     );
   }
 }
